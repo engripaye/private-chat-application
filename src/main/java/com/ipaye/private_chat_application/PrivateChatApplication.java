2aa6a51d928d6bac949e95deed4e56da52e18694
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-01-28 03:03:32 +0100

    init commit

---
+++ src/main/java/com/ipaye/private_chat_application/PrivateChatApplication.java
@@ -0,0 +1,13 @@
+package com.ipaye.private_chat_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ipaye/private_chat_application/PrivateChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipaye.private_chat_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
